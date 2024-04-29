88a5a75afa1ca00bc476ae11b4fb518aace799c1
emilyyf <emily@anaboth.com>
2024-04-29 18:42:20 -0300

    feat: add basic repl

--- main.go
+++ main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"monki/repl"
+	"os"
+	"os/user"
 )
 
 func main() {
-	fmt.Printf("Hi")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hey %s! This is a basic REPL for the Monkey progamming language!\n", user.Username)
+	fmt.Printf("Feel free to type some commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
