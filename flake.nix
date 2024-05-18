67510303b3649bcad40be9658b7ce74cdc73d9ce
Emily <emily@anaboth.com>
2024-05-18 19:34:59 -0300

    feat: change nix shell to flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Flake for building monki";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+            zsh
+          ];
+
+          shellHook = ''
+            exec zsh
+          '';
+        };
+      }
+    );
+}
